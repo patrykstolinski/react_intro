de3aec7bfd08c2cab0528c8c94268e862554d992
Patryk Stolinski <patrykstolinski@gmail.com>
2025-08-19 12:51:26 +0200

    feat: Adding inline style for character and quote elements.

--- got-quote-generator/src/App.js
+++ got-quote-generator/src/App.js
@@ -3,6 +3,28 @@ import './App.css';
 const quote = "Der Winter naht.";
 const character = "Ned Stark";
 
+const quoteStyle = {
+  fontStyle: "italic",
+  color: "#F8C471",
+  fontSize: "1.5em",
+  marginBottom: "10px",
+  borderLeft: "4px solid #F8C471",
+  paddingLeft: "15px",
+  display: "inline-block",
+};
+
+const characterStyle = {
+    fontSize: '0.8em',
+    color: '#ccc',
+    marginTop: '5px',
+    display: 'block'
+  };
+
+  const quoteBg = {
+    backgroundColor: "#884c4cff"
+
+  }
+
 
 function App() {
   return (
@@ -10,13 +32,17 @@ function App() {
       <header className="App-header">
       <h1>Game of Thrones Zitat-generator</h1>
       <p>Ein Ort für Weisheit (und Sarkasmus) aus Westeros.</p>
+      </header>
+      <div style={quoteBg}>
       <main>
-        <blockquote>
+        <blockquote style={quoteStyle}>
           "{quote}"
         </blockquote>
       </main>
-      <footer>- {character} </footer>
-      </header>
+      <footer style={characterStyle}>
+        - {character}
+      </footer>
+      </div>
     </div>
   );
 }
