fd828f3794843720a52e12324b7bac14124961db
Patryk Stolinski <patrykstolinski@gmail.com>
2025-08-19 11:57:24 +0200

    feat: Added blockquote and footer elements.

--- got-quote-generator/src/App.js
+++ got-quote-generator/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
+const quote = "Der Winter naht.";
+const character = "Ned Stark";
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>Game of Thrones Zitat-generator</h1>
       <p>Ein Ort für Weisheit (und Sarkasmus) aus Westeros.</p>
+      <main>
+        <blockquote>
+          "{quote}"
+        </blockquote>
+      </main>
+      <footer>- {character} </footer>
       </header>
     </div>
   );
